f431d95c8bf0abc06743c18be1eab6b4c93f3c13
Muneeb Mukhtar <muneeb.mukhtar@srijan.net>
2023-09-19 17:24:59 +0530

    initial commit

---
+++ js/digital-solutions.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * digital_solutions behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.digitalSolutions = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
